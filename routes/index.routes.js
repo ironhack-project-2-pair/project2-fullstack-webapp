0439719e2642609b189b5d2062c1c2b57cd3778e
Philippe Miossec <pmiossec@gmail.com>
2023-06-09 22:29:33 +0200

    feat(page-feed): display an multiple feeds contents

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,20 +1,64 @@
 const express = require('express');
 const router = express.Router();
 const { parse } = require('rss-to-json');
+const isLoggedIn = require('../middleware/isLoggedIn');
+const Feed = require('../models/Feed.model');
 
 /* GET home page */
 router.get("/", async (req, res, next) => {
-
+  console.log("req.session.currentUser", req.session.currentUser);
+  if(!req.session.currentUser) {
+    res.render("index", { });
+    return;
+  }
+  
   try {
-    var rss = await parse('https://www.lemonde.fr/rss/une.xml');
-    // res.send('content feed:' + JSON.stringify(rss, null, 3));
-    // console.log(JSON.stringify(rss, null, 3));
-    res.render("index", { feedContent : rss });
+      // console.log("Get feeds");
+      const feedContent = [];
+      const feeds = await Feed.find();
+      //console.log("feeddd:", feeds);
+      for (let i = 0; i < feeds.length; i++) {
+        const feed = feeds[i];
+        // console.log("url:", feed.url);
+        const content = await parse(feed.url);
+        //console.log("content:", content);
+        
+        const now = new Date();
+        content.items.forEach(i => i.formatedDate = formatDate(new Date(i.published), now))
+        feedContent.push(content);
+      }
+
+      // console.log("feeds", feedContent);
+      res.render("index", { feeds : feedContent });
 
   } catch (error) {
+    console.log('error', error);
     res.send('error parsing feed:' + error);
   }
   // res.render("index");
 });
 
 module.exports = router;
+
+
+const formatDate = (date, now) => {
+  if (!date) {
+    return '-';
+  }
+  const nowDay = now.getDate();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const year = date.getFullYear();
+  if (now.getFullYear() - year > 1)
+  {
+    return `${padDigits(month)}/${padDigits(year)}`
+  }
+
+  return nowDay <= day && now.getMonth() === month
+    ? `${padDigits(date.getHours())}:${padDigits(date.getMinutes())}`
+    : `${padDigits(day)}/${padDigits(month + 1)}`;
+};
+
+const padDigits = (num, digits = 2) => {
+  return Array(Math.max(digits - String(num).length + 1, 0)).join('0') + num;
+};
